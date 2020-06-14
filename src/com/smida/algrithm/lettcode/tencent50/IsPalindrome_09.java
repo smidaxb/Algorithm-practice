435149f257fa4fff86674bf20a5b6bd3e863b84c
yangyifan <yangyifan@sunlands.com>
2020-06-15 00:39:32 +0800

    t05

--- src/com/smida/algrithm/lettcode/tencent50/IsPalindrome_09.java
+++ src/com/smida/algrithm/lettcode/tencent50/IsPalindrome_09.java
@@ -20,14 +20,49 @@ package com.smida.algrithm.lettcode.tencent50;
  */
 public class IsPalindrome_09 {
     public static boolean isPalindrome(int x) {
-
+        if (x < 0) {
+            return false;
+        }
+        char[] arr = String.valueOf(x).toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
+
     /**
-     *   进阶:
-     *
-     *   你能不将整数转为字符串来解决这个问题吗？
+     * 进阶:
+     * <p>
+     * 你能不将整数转为字符串来解决这个问题吗？
      */
     public static boolean isPalindromeWithOutStr(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int num = 0;
+        int tmp = x;
+        while (tmp != 0) {
+            num = 10 * num + (tmp % 10);
+            tmp = tmp/10;
+        }
+        return num == x;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+        System.out.println(isPalindrome(0));
+        System.out.println("==============================================================");
+        System.out.println(isPalindromeWithOutStr(121));
+        System.out.println(isPalindromeWithOutStr(-121));
+        System.out.println(isPalindromeWithOutStr(10));
+        System.out.println(isPalindromeWithOutStr(0));
     }
 }
