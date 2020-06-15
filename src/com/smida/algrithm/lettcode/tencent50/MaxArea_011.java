fab7bc6d6de9307f40aa7d5ecb80036d2a33332d
yangyifan <yangyifan@sunlands.com>
2020-06-15 19:23:54 +0800

    t 06

---
+++ src/com/smida/algrithm/lettcode/tencent50/MaxArea_011.java
@@ -0,0 +1,49 @@
+package com.smida.algrithm.lettcode.tencent50;
+
+/**
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 示例：
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ */
+public class MaxArea_011 {
+    public static int maxArea(int[] height) {
+        int res = 0;
+        if (null == height || height.length < 2) {
+            return res;
+        }
+        for (int i = 1; i < height.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int volume = Math.min(height[i], height[j]) * (i - j);
+                res = volume > res ? volume : res;
+            }
+        }
+        return res;
+    }
+
+    public static int maxAreaON(int[] height) {
+        int res = 0;
+        if (null == height || height.length < 2) {
+            return res;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int volume = Math.min(height[left], height[right]) * (right - left);
+            res = volume > res ? volume : res;
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(maxAreaON(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
